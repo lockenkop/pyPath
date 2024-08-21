d5b14eaa7dd6fd32fd0002e5468802337614fe42
Jonas Fenchel <locken.kop97@gmail.com>
2024-08-21 22:12:58 +0200

    ensure .venv

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 
-
-python -m venv .venv
+# make sure we are in a virtual environment if not create it and activate it
+if [ -z "$VIRTUAL_ENV" ]; then
+    python3 -m venv venv
+    source venv/bin/activate
+fi
 
 source .venv/bin/activate
 
